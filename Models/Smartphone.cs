de6e3d516137b6e10ed81ea5017a1d2dab2481b2
Rodrigo Paulino <rodrigo.paulino@atos.net>
2023-01-20 16:39:40 -0300

    Teste

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,6 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria  { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero)
