1dcd39664449088e3c524e5b36a244321a038936
Rodrigo Paulino <rodrigo.paulino@atos.net>
2023-01-23 11:59:02 -0300

    teste 2

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(int memoria) : base(memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,16 +2,22 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
-        public int Memoria  { get; set; }
+        protected string Numero { get; set; }
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria  { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        
+        public Smartphone(string numero,string modelo, string imei)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
             // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+        public Smartphone(int memoria)
+        {
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -24,6 +30,6 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstalarAplicativo(string nomeApp); 
     }
 }
